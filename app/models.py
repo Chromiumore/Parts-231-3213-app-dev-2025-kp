b026a009e9a5f30d0ab2b1e16f9b1563bd9131b9
Chromiumore <parts.dima06@gmail.com>
2025-05-21 22:27:16 +0300

    models

---
+++ app/models.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+import os
+
+from flask import url_for
+from flask_login import UserMixin
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import MetaData, String, ForeignKey, Text
+from flask_sqlalchemy import SQLAlchemy
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": 'ix_%(column_0_label)s',
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
+
+db = SQLAlchemy(model_class=Base)
+
+class User(Base, UserMixin):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(25), unique=True)
+    password_hash: Mapped[str] = mapped_column(String(256))
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    role_id: Mapped[int] = mapped_column(ForeignKey('roles.id'))
+
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(40))
+    description: Mapped[str] = mapped_column(Text())
+
+class Game(Base):
+    __table__ = 'games'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    description: Mapped[str] = mapped_column(String(150))
+    info: Mapped[str] = mapped_column(Text())
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+    last_updated_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+# TODO Модель статистики
